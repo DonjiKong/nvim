f5df161537ad4b220db658bf1dedd4bc67bdbe2e
donjikong <theodoreyd@gmailcom>
2023-05-25 00:56:13 -0700

    2023 neovim config 2

--- init.lua
+++ init.lua
@@ -5,13 +5,4 @@ require("donjikong")
 
 vim.o.winbar = "%{%v:lua.require('donjikong.win').eval()%}"
 
--- If installed as an optional package.
-vim.cmd.packadd('nvim-treeclimber')
-
--- Setup and configuration.
-require('nvim-treeclimber').setup()
-
--- Rest of your configuration.
--- ...
-
 
